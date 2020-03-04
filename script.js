28eca1e49d4128fd3f7521f60011f84b6f0c0f66
mustafa idoufkir <mustafa.idoufkir@gmail.com>
2020-03-04 15:04:39 +0100

    second+ commit

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function Add1() {
 
         var convers = document.getElementById("conversation");
         var outp = document.createElement("p");
+
+        outp.setAttribute('class','messages1')
+
         var outpmaster = document.createTextNode(message.value);
 
             outp.appendChild(outpmaster);       
@@ -19,7 +22,7 @@ function Add2() {
         var convers = document.getElementById("conversation");
         var outp = document.createElement("p");
 
-        outp.setAttribute('class','messages')
+        outp.setAttribute('class','messages2 bubble--alt')
 
         var outpmaster = document.createTextNode(message.value);
 
@@ -27,3 +30,21 @@ function Add2() {
         convers.appendChild(outp);       
     
 }
+
+//Time
+
+/*
+var deviceTime = document.querySelector('.messages1 .messages2 .time');
+var messageTime = document.querySelectorAll('.messages1 .messages2 .time');
+
+deviceTime.innerHTML = moment().format('h:mm');
+
+setInterval(function() {
+	deviceTime.innerHTML = moment().format('h:mm');
+}, 1000);
+
+for (var i = 0; i < messageTime.length; i++) {
+	messageTime[i].innerHTML = moment().format('h:mm A');
+}
+
+*/
